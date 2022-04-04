a5866cfe597ab87797a7ecd555cf05ca449bed05
LMDGenesis <ldiliberto@genesistrading.com>
2022-04-04 13:08:01 -0400

    Transactions
    Am able to create transactions on the test net using Javascript

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 var ethers = require('ethers');  
 var crypto = require('crypto');
+var bip39 = require('bip39');
 
+//Create Mnemonic
+//const mnemonic = bip39.generateMnemonic(256)
+//console.log(mnemonic)
+const mnemonicSaved = "copy vast such slogan life educate meat bitter bus grief survey trip reopen scrap north swing write arm celery exit quit fork vintage praise"
 
-//Creates a random 32 byte string in hex
-var id = crypto.randomBytes(32).toString('hex');
+//Create a wallet based on a mnemonic
+var walletHard = new ethers.Wallet.fromMnemonic(mnemonicSaved);
+console.log("Address: " + walletHard.address);
 
-//Creates a private key by adding eth's sig on top of 0x
-var privateKey = "0x"+id;
-console.log("SAVE BUT DO NOT SHARE THIS:", privateKey);
+//Connect Wallet to Ropsten Test-Net
+wallet = walletHard.connect(new ethers.providers.InfuraProvider("ropsten"));
 
-var wallet = new ethers.Wallet(privateKey);
-console.log("Address: " + wallet.address);
+//Creating a transaction
+tx = {
+    to: "0xe939557dc16341eC621DbE1c752f54cDB28f9c85",
+    value: ethers.utils.parseEther("0.00001")
+}
+console.log("Created Transaction!")
+
+//Sign transaction
+wallet.signTransaction(tx)
+console.log("Signing Transaction!")
+
+//Send a transaction
+wallet.sendTransaction(tx);
+console.log("Sent Transaction!")
